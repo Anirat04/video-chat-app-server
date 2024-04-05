9d316a0363baff3994e6ed251c0388a2d41bee98
Tanbir Haque <tanbirhaque53@gmail.com>
2024-04-06 03:27:26 +0600

    Answer function return from the called user

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ io.on('connection', socket => {
         const socketId = emailToSocketMapping.get(emailId);
         socket.to(socketId).emit('incoming-call', {from: fromEmail, offer})
     })
+
+    socket.on('call-accepted', (data) => {
+        const {emailId, ans} = data;
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('call-accepted', {ans})
+    })
 });
 
 app.listen(8000, () => console.log('Http server running on PORT 8000'))
