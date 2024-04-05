f8c8854d2ac4bf342513d4d754f0711635b7c08b
Tanbir Haque <tanbirhaque53@gmail.com>
2024-04-06 01:40:42 +0600

    Incomming call added by the second user implemented

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 
-const io = new Server()
+const io = new Server({
+    cors: true,
+})
 const app = express();
 
 
 app.use(bodyParser.json());
 
+const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
+
 io.on('connection', socket => {
-    
-})
+    console.log("New connection");
+
+    socket.on('join-room', (data) => {
+        const { roomId, emailId } = data;
+        console.log('User', emailId, 'Joined room', roomId);
+        emailToSocketMapping.set(emailId, socket.id);
+        socketToEmailMapping.set(socket.id, emailId);
+        socket.join(roomId);
+        socket.emit('joined-room', {roomId});
+        socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    });
+
+    socket.on('call-user', (data) => {
+        const {emailId, offer} = data;
+        const fromEmail = socketToEmailMapping.get(socket.id)
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('incoming-call', {from: fromEmail, offer})
+    })
+});
 
 app.listen(8000, () => console.log('Http server running on PORT 8000'))
 io.listen(8001)
